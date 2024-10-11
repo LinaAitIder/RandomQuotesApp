f81a4e64886c9e5b6eb30b15e14088c9ef93dbcb
lina <linaitider@gmail.com>
2024-10-11 20:56:06 +0100

    API integration

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from "react";
 import axios from "axios";
 import "./App.css";
 
+// const options = {
+//   method: 'GET',
+//   url: 'https://api.api-ninjas.com/v1/quotes',
+//   headers: {
+//     'X-Api-Key': 'oZOap4y044E7DdfoPaolzg==tiwdPhmRNgyccii4'
+//   }
+// };
+
 class App extends React.Component {
   state = { 
     content: "",  // Add 'content' field to store the quote
@@ -13,14 +21,26 @@ class App extends React.Component {
     }
 
     fetchAdvice = () => {
-        axios.get("http://api.quotable.io/random")
-            .then((response) => {
-                const { content, author } = response.data;
-                this.setState({ content , author });
-            })
-            .catch((error) => {
+        // axios.get('https://api.quotable.io/random')
+        // .then((response) => {
+        //         const { content, author } = response.data;
+        //         this.setState({ content , author });
+        //         console.log(response);
+        //     })
+        axios.get('https://api.api-ninjas.com/v1/quotes', {
+          headers: {
+            'X-Api-Key': 'oZOap4y044E7DdfoPaolzg==tiwdPhmRNgyccii4'
+          },
+          timeout: 10000 // 10 seconds
+        })
+        .then(response => {
+          const { quote , author } = response.data[0];
+         this.setState({ content: quote, author });
+          console.log(response.data);
+        })
+        .catch((error) => {
                 console.log(error);
-            });
+        });
     };
 
     render() {
@@ -29,8 +49,8 @@ class App extends React.Component {
         return (
           <div className="app">
             <div className="quote-box">
-                    <p className="text">{content}</p>
-                    <p className="author">{author}</p>
+                    <p className="text">"{content}"</p>
+                    <p className="author">-{author}</p>
                     <button className="new-quote" onClick={this.fetchAdvice}>
                         <span>Give Me Advice</span>
                     </button>
