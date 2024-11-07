9ad40ba8fe2ac599f35c034aef191b976b107d84
lina <linaitider@gmail.com>
2024-11-07 23:10:15 +0100

    Changing API quotable to ninjas + Some Changes on the way we get the Data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from "react";
 import axios from "axios";
 import "./App.css";
-
-// const options = {
-//   method: 'GET',
-//   url: 'https://api.api-ninjas.com/v1/quotes',
-//   headers: {
-//     'X-Api-Key': 'oZOap4y044E7DdfoPaolzg==tiwdPhmRNgyccii4'
-//   }
-// };
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'; 
 
 class App extends React.Component {
+  bgColors = ["#697565","#3C3D37", '#AB886D',"#E4E0E1","#789DBC", "#FFB0B0"];
   state = { 
-    content: "",  // Add 'content' field to store the quote
-    author: ""    // Add 'author' field to store the author name
+    content: "Fear is the foundation of most governments.", 
+    author: "John Adams"    
   };
 
     componentDidMount() {
@@ -21,39 +16,45 @@ class App extends React.Component {
     }
 
     fetchAdvice = () => {
-        // axios.get('https://api.quotable.io/random')
-        // .then((response) => {
-        //         const { content, author } = response.data;
-        //         this.setState({ content , author });
-        //         console.log(response);
-        //     })
-        axios.get('https://api.api-ninjas.com/v1/quotes', {
+        axios.get("https://api.api-ninjas.com/v1/quotes", {
           headers: {
             'X-Api-Key': 'oZOap4y044E7DdfoPaolzg==tiwdPhmRNgyccii4'
           },
-          timeout: 10000 // 10 seconds
+          timeout: 10000 
         })
         .then(response => {
           const { quote , author } = response.data[0];
          this.setState({ content: quote, author });
-          console.log(response.data);
+          console.log(response.data[0]);
         })
         .catch((error) => {
                 console.log(error);
         });
     };
+  
+  changeBg = () => {
+     const randomColor = this.bgColors[Math.floor(Math.random() * this.bgColors.length)];
+    document.body.style.backgroundColor = randomColor;
+  }
+
+  
 
     render() {
         const { content , author } = this.state;
+        const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(content)} - ${encodeURIComponent(author)}`;
+
 
         return (
-          <div className="app">
-            <div className="quote-box">
-                    <p className="text">"{content}"</p>
-                    <p className="author">-{author}</p>
-                    <button className="new-quote" onClick={this.fetchAdvice}>
-                        <span>Give Me Advice</span>
+          <div className="app">                               
+            <h1>Quote Generator</h1>
+            <div id="quote-box">
+                    <i class="bi bi-quote fs-1"></i>
+                    <p id="text" className="hl-1">{content}</p>
+                    <p id="author" className="text-center">-{author}</p>
+                    <button id="new-quote" className="btn btn-light m-2" onClick={()=>{this.fetchAdvice(); this.changeBg();}}>
+                        <span>New Quote</span>
                     </button>
+                    <a id="tweet-quote" href={tweetUrl}  target="_blank" rel="noopener noreferrer" className="text-center btn-primary btn d-flex align-items-center justify-content-center"><i class="bi bi-twitter-x p-0 m-0"></i></a>
                 </div>
         </div>
         );
